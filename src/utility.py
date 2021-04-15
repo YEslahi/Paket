720764a944554ae996774a8d6d97ee6763b1cf11
YEslahi <yasi.e72@gmail.com>
2021-04-15 16:39:16 +0200

    added to utility the scripts to validate the jpeg images. needs refactoring

--- src/utility.py
+++ src/utility.py
@@ -20,6 +20,41 @@ def plot_images(images_arr):
     plt.tight_layout()
     plt.show()
 
+def is_image(folder, filename, verbose=True):
+
+    data = open(folder+filename,'rb').read(10)
+    # check if file is JPG or JPEG
+    if data[:3] == b'\xff\xd8\xff':
+        if verbose == True:
+             print(filename+" is: JPG/JPEG.")
+        return True
+
+    # check if file is PNG
+    if data[:8] == b'\x89\x50\x4e\x47\x0d\x0a\x1a\x0a':
+        if verbose == True:
+             print(filename+" is: PNG.")
+        return True
+
+    # check if file is GIF
+    if data[:6] in [b'\x47\x49\x46\x38\x37\x61', b'\x47\x49\x46\x38\x39\x61']:
+        if verbose == True:
+             print(filename+" is: GIF.")
+        return True
+
+    return False
+def validate_image_type(folder = '../data/img/all_images/'):
+    """get a folder with images and check whether they are from type JPG/JPEG, PNG, or GIF.
+    if not, the photo will be removed from the directory!"""
+    
+    # go through all files in desired folder
+    for filename in os.listdir(folder):
+         # check if file is actually an image file
+         if is_image(folder, filename, verbose=True) == False:
+              # if the file is not valid, remove it
+              print("Warning! your photo is being removed from the directory!")
+              import code; code.interact(local=dict(globals(), **locals()))
+              os.remove(os. path. join(folder, filename))
+
 # validate image files
 def validate_image_file(folder_path):
     '''
@@ -127,3 +162,66 @@ def dir_file_cnt(dir_path):
     path, dirs, files = next(os.walk(dir_path))
     file_count = len(files)
     return file_count
+
+
+
+# # another validaate type start        
+# #-------------------------------
+# # TOdo: clean this up and make it a function.
+# from struct import unpack
+# import os
+# # from tqdm import tqdm
+# import os.path as osp
+
+# folder = '../data/img/all_images/'
+# images = os.listdir(folder)
+# marker_mapping = {
+#     0xffd8: "Start of Image",
+#     0xffe0: "Application Default Header",
+#     0xffdb: "Quantization Table",
+#     0xffc0: "Start of Frame",
+#     0xffc4: "Define Huffman Table",
+#     0xffda: "Start of Scan",
+#     0xffd9: "End of Image"
+# }
+
+
+# class JPEG:
+#     def __init__(self, image_file):
+#         with open(image_file, 'rb') as f:
+#             self.img_data = f.read()
+    
+#     def decode(self):
+#         data = self.img_data
+#         while(True):
+#             marker, = unpack(">H", data[0:2])
+#             # print(marker_mapping.get(marker))
+#             if marker == 0xffd8:
+#                 data = data[2:]
+#             elif marker == 0xffd9:
+#                 return
+#             elif marker == 0xffda:
+#                 data = data[-2:]
+#             else:
+#                 lenchunk, = unpack(">H", data[2:4])
+#                 data = data[2+lenchunk:]            
+#             if len(data)==0:
+#                 break        
+
+
+# bads = []
+
+# for img in tqdm(images):
+#   image = osp.join(folder,img)
+#   image = JPEG(image) 
+#   try:
+#     image.decode()   
+#   except:
+#     bads.append(img)
+
+
+# for name in bads:
+#     import code; code.interact(local=dict(globals(), **locals()))
+#     os.remove(osp.join(folder, name))
+# # another validaate type end
+# #-------------------------------
